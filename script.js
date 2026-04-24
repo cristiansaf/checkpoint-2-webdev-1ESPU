5fd4ef8eb3a0d45afaad010b4699ca08b9e51c0a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-23 22:38:59 -0300

    questao 7

--- script.js
+++ script.js
@@ -30,6 +30,21 @@ else if (prioridade === 3) {
 // Questão 6
 
 // Questão 7
+function verificarIdade(idade) {
+    if (idade < 18) {
+        return "Menor de idade";
+    } else if (idade >= 18 && idade < 60) {
+        return "Maior de idade";
+    } else {
+        return "Idoso";
+    }
+}
+
+console.log(verificarIdade(10));
+console.log(verificarIdade(18));
+console.log(verificarIdade(35));
+console.log(verificarIdade(60));
+console.log(verificarIdade(75));
 
 
 // Questão 8
