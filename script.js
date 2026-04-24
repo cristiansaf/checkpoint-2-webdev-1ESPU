7b36a8b96ee3435f3607bd61b9e42df0c9d8a3b4
Bolo-ctrl <deepgreenolives@protonmail.com>
2026-04-23 22:11:34 -0300

    "Arrumando no teste"

---
+++ script.js
@@ -0,0 +1,39 @@
+// Questão 1
+let tarefaConcluida = true;
+if (tarefaConcluida === true) {
+    console.log("Parabéns, você concluiu a tarefa!");
+}
+else {
+    ("A tarefa não foi concluída.")
+}
+
+// Questão 2
+
+
+// Questão 3
+
+// Questão 4
+
+
+
+// Questão 5
+
+// Questão 6
+
+// Questão 7
+
+
+// Questão 8
+
+
+
+// Questão 9
+let tarefas = ["Estudar","htmlcss","python","javascript","c#"]
+
+tarefas_separadas = tarefas.split(",")
+console.log(tarefas_separadas)
+
+
+// Questão 10 
+
+
